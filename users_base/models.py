f73d21a2653ac08aeb2208c32c92dc09a093e6d2
Michal <michal0468@gmail.com>
2015-06-29 15:22:24 +0200

    Added model Users

--- users_base/models.py
+++ users_base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    birthday = models.DateField()
+    random_number = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Użytkownik"
+        verbose_name_plural = "Użytkownicy"
+
+    def __unicode__(self):
+        return self.first_name+" "+self.last_name
