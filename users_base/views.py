7a3e527b27a046189deb669f05ea8777b054072a
Michal <michal0468@gmail.com>
2015-06-29 20:43:01 +0200

    editing user values

--- users_base/views.py
+++ users_base/views.py
@@ -14,7 +14,6 @@ def add(request):
         form_user.is_valid()
         data = form_user.cleaned_data
         if form_user.is_valid()==True:
-            print data['first_name'], data['last_name'], data['birthday']
             obj = User(
                 first_name=data['first_name'],
                 last_name=data['last_name'],
@@ -26,3 +25,35 @@ def add(request):
     return render_to_response(
         'add.html', {'form': UserForm},
         context_instance=RequestContext(request))
+
+
+def remove(request):
+    if request.method == "GET":
+        print request.GET.get('id')
+        User.objects.get(id=request.GET.get('id')).delete()
+    return HttpResponseRedirect('/')
+
+
+def edit(request):
+    if request.method == "GET":
+        user_for_id = User.objects.get(id=request.GET.get('id'))
+        UserFormEdit = UserForm(
+            {'first_name': user_for_id.first_name,
+             'last_name': user_for_id.last_name,
+             'birthday': user_for_id.birthday}
+             )
+        return render_to_response(
+            'edit.html', {'form': UserFormEdit},
+            context_instance=RequestContext(request))
+    if request.method == "POST":
+        user_for_id = User.objects.get(id=request.GET.get('id'))
+        print user_for_id
+        form_user = UserForm(request.POST)
+        form_user.is_valid()
+        data = form_user.cleaned_data
+        if form_user.is_valid()==True:
+            user_for_id.first_name = data['first_name']
+            user_for_id.last_name = data['last_name']
+            user_for_id.birthday = data['birthday']
+            user_for_id.save()
+        return HttpResponseRedirect('/')
--- zadanie_django/urls.py
+++ zadanie_django/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
         url(r'^/?$', 'users_base.views.home'),
         url(r'^add$', 'users_base.views.add'),
+        url(r'^remove$', 'users_base.views.remove'),
+        url(r'^edit$', 'users_base.views.edit'),
 )
