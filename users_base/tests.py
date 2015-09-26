9477bd0405c464cb12c14675029c3798ee6ffee0
Michal <michal0468@gmail.com>
2015-09-26 17:35:07 +0200

    added tests

--- users_base/tests.py
+++ users_base/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from users_base.models import User
 
-# Create your tests here.
+
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(first_name="Janek", last_name="Kowalski", birthday="1900-11-22")
+        User.objects.create(first_name="Janina", last_name="Kowalska", birthday="1990-10-20")
+
+    def test_usermodel(self):
+        one = User.objects.get(first_name="Janek")
+        two = User.objects.get(first_name="Janina")
+        self.assertEqual(one.last_name, "Kowalski")
+        self.assertEqual(two.last_name, "Kowalska")
