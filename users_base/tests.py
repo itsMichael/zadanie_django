5d3fef440b334f409d48ffbbaa3c98336150aad9
Michał Rosiak <michal@MacBook-Pro-Micha.local>
2015-10-12 23:07:03 +0200

    tests

--- users_base/tests.py
+++ users_base/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from users_base.models import User
-import unittest
 from django.test import Client
 from django.core.urlresolvers import reverse
 
@@ -10,14 +9,14 @@ class UserModelTestCase(TestCase):
         User.objects.create(first_name="Janek", last_name="Kowalski", birthday="1900-11-22")
         User.objects.create(first_name="Janina", last_name="Kowalska", birthday="1990-10-20")
 
-    def test_usermodel(self):
+    def test_details(self):
         self.one = User.objects.get(first_name="Janek")
         self.two = User.objects.get(first_name="Janina")
         self.assertEqual(self.one.last_name, "Kowalski")
         self.assertEqual(self.two.last_name, "Kowalska")
 
 
-class ViewIndexTest(unittest.TestCase):
+class ViewIndexTest(TestCase):
     def setUp(self):
         self.client = Client()
         User.objects.create(first_name="Stefan", last_name="Cebula", birthday="1994-10-20")
@@ -31,10 +30,35 @@ class ViewIndexTest(unittest.TestCase):
         self.assertEqual([entry.first_name for entry in self.response.context['object_list']], ["Stefan", "Slawomir"])
 
 
-class ViewAddTest(unittest.TestCase):
+class ViewAddTest(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_details(self):
         self.client.post(reverse('add'), {'first_name': "jan", 'last_name': 'janowski', 'birthday': '1900-10-22'})
         self.assertEqual(User.objects.count(), 1)
+
+
+"""class ViewEditTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        User.objects.create(first_name="Stefan", last_name="Cebula", birthday="1994-10-20")
+
+    def test_details(self):
+        self.client.post("/edit/1", {'first_name': "jan", 'last_name': 'janowski', 'birthday': '1900-10-22'})
+        self.assertEqual(User.objects.all()[0].first_name, "jan")"""
+
+
+class ViewDeleteTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.myuser = User.objects.create(first_name="Stefan", last_name="Cebula", birthday="1994-10-20")
+
+    def test_my_get_request(self):
+        response = self.client.get(reverse('remove', args=(self.myuser.id,)), follow=True)
+        self.assertContains(response, 'Are you sure you want to delete') # THIS PART WORKS
+
+    def test_my_post_request(self):
+        post_response = self.client.post(reverse('remove', args=(self.myuser.id,)), follow=True)
+        self.assertRedirects(post_response, reverse('remove'), status_code=302)
+        self.assertEqual(User.objects.count(), 0)
--- users_base/urls.py
+++ users_base/urls.py
@@ -5,7 +5,7 @@ from users_base.views import AddUser, DeleteUser, UpdateUser, Home
 urlpatterns = [
     url(r'^/?$', Home.as_view(), name='home'),
     url(r'^add$', AddUser.as_view(), name="add"),
-    url(r'^remove/(?P<pk>[0-9]+)/$', DeleteUser.as_view()),
+    url(r'^remove/(?P<pk>[0-9]+)/$', DeleteUser.as_view(), name="remove"),
     url(r'^edit/(?P<pk>[0-9]+)/$', UpdateUser.as_view()),
     url(r'^generate$', 'users_base.views.generate_csv', name="generate"),
 ]
